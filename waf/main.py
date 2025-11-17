b86251745d538324865b3b70b1518aeb492de785
Darrius-W <darriuswhite435@gmail.com>
2025-11-16 18:50:41 -0600

    Implemented waf entrypoint route to accept requests regardless of given path or method

---
+++ waf/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, Request
+from waf.pipeline import run_pipeline
+
+app = FastAPI()
+
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+async def waf_entrypoint(request: Request, path: str):
+    return run_pipeline(request)
